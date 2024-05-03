a4e46a8368c6ac5f75d0ee6b92afc0d3508c353b
Akhil Kumar <127279064+akhilkumar1101@users.noreply.github.com>
2024-05-03 16:20:07 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '728ba5853dmsh0f6514eff0df1e9p121de6jsncf74db7aa5d4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1abe2212e419fa3f40013e186553bab8";
